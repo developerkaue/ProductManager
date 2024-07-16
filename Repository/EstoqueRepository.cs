48e12dea2ab26983cef973fa6713161ec49ba04d
developerkaue <kauecaobiancofernandes2@gmail.com>
2024-07-16 09:19:25 -0300

    Example using stored procedure

--- Repository/EstoqueRepository.cs
+++ Repository/EstoqueRepository.cs
@@ -1,9 +1,10 @@
-﻿using ProductManagementSystem1.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagementSystem1.Data;
 using ProductManagementSystem1.Interface;
 using ProductManagementSystem1.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Data.SqlClient;
-
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
 
 namespace ProductManagementSystem1.Repository
 {
@@ -18,50 +19,36 @@ namespace ProductManagementSystem1.Repository
 
         public async Task<IEnumerable<Estoque>> GetAllAsync()
         {
-            return await _context.Estoques
-                .Include(e => e.Produto)
-                .Include(e => e.Fornecedor)
-                .ToListAsync();
+            return await _context.Estoques.FromSqlRaw("EXEC spGetAllEstoques").ToListAsync();
         }
 
         public async Task<Estoque> GetByIdAsync(int id)
         {
-            return await _context.Estoques
-                .Include(e => e.Produto)
-                .Include(e => e.Fornecedor)
-                .FirstOrDefaultAsync(e => e.Id == id);
+            var estoque = await _context.Estoques.FromSqlRaw("EXEC spGetEstoqueById @Id={0}", id).FirstOrDefaultAsync();
+            return estoque;
         }
 
         public async Task AddAsync(Estoque estoque)
         {
-
-            _context.Estoques.Add(estoque);
-            await _context.SaveChangesAsync();
+            await _context.Database.ExecuteSqlRawAsync("EXEC spAddEstoque @ProdutoId={0}, @FornecedorId={1}, @Quantidade={2}",
+                estoque.ProdutoId, estoque.FornecedorId, estoque.Quantidade);
         }
-
 
         public async Task UpdateAsync(Estoque estoque)
         {
-            _context.Estoques.Update(estoque);
-            await _context.SaveChangesAsync();
+            await _context.Database.ExecuteSqlRawAsync("EXEC spUpdateEstoque @Id={0}, @ProdutoId={1}, @FornecedorId={2}, @Quantidade={3}",
+                estoque.Id, estoque.ProdutoId, estoque.FornecedorId, estoque.Quantidade);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _context.Database.ExecuteSqlRawAsync("EXEC spDeleteEstoque @Id={0}", id);
         }
 
         public async Task AtualizarAsync(Estoque estoque)
         {
-            _context.Entry(estoque).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
-        }
-
-
-        public async Task DeleteAsync(int id)
-        {
-            var estoque = await _context.Estoques.FindAsync(id);
-            if (estoque != null)
-            {
-                _context.Estoques.Remove(estoque);
-                await _context.SaveChangesAsync();
-            }
+            await _context.Database.ExecuteSqlRawAsync("EXEC spUpdateEstoque @Id={0}, @ProdutoId={1}, @FornecedorId={2}, @Quantidade={3}",
+                estoque.Id, estoque.ProdutoId, estoque.FornecedorId, estoque.Quantidade);
         }
     }
-
 }
--- Repository/ProdutoRepository.cs
+++ Repository/ProdutoRepository.cs
@@ -1,9 +1,9 @@
-﻿using ProductManagementSystem1.Interface;
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagementSystem1.Data;
+using ProductManagementSystem1.Interface;
 using ProductManagementSystem1.Models;
-using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using ProductManagementSystem1.Data;
 
 namespace ProductManagementSystem1.Repository
 {
@@ -16,26 +16,32 @@ namespace ProductManagementSystem1.Repository
             _context = context;
         }
 
-        public async Task AtualizarAsync(Product produto)
+        public async Task<IEnumerable<Product>> GetAllAsync()
         {
-            _context.Update(produto);
-            await _context.SaveChangesAsync();
+            return await _context.Products.FromSqlRaw("EXEC spGetAllProdutos").ToListAsync();
         }
 
         public async Task<Product> GetByIdAsync(int id)
         {
-            return await _context.Products.FindAsync(id);
+            var produto = await _context.Products.FromSqlRaw("EXEC spGetProdutoById @Id={0}", id).FirstOrDefaultAsync();
+            return produto;
         }
 
-        public async Task AdicionarAsync(Product produto)
+        public async Task AddAsync(Product produto)
         {
-            _context.Add(produto);
-            await _context.SaveChangesAsync();
+            await _context.Database.ExecuteSqlRawAsync("EXEC spAddProduto @Nome={0}, @Descricao={1}, @Preco={2}, @FornecedorId={3}",
+                produto.Nome, produto.Descricao, produto.Preco, produto.FornecedorId);
         }
 
-        public async Task<IEnumerable<Product>> GetAllAsync()
+        public async Task UpdateAsync(Product produto)
         {
-            return await _context.Products.ToListAsync();
+            await _context.Database.ExecuteSqlRawAsync("EXEC spUpdateProduto @Id={0}, @Nome={1}, @Descricao={2}, @Preco={3}, @FornecedorId={4}",
+                produto.Id, produto.Nome, produto.Descricao, produto.Preco, produto.FornecedorId);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _context.Database.ExecuteSqlRawAsync("EXEC spDeleteProduto @Id={0}", id);
         }
     }
 }
